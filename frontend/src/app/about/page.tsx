52862a2947c766cf7a69e53edf66d537344bb06b
kimzii <kimzietorres@gmail.com>
2023-12-17 23:13:26 +0800

    edited about

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,11 +1,19 @@
 function AboutPage() {
+    
+    
     return ( 
-        <main className="prose mx-auto">
-            <div className="border-2 border-red rounded-lg p-4">
+        <main className="prose mx-auto border-2 border-red rounded-lg p-4">
+            <div>
                 <h2 className="font-bold text-center">Welcome to USePinion</h2>
                 <p className="text-justify">
                     Where student opinions take center stage! Our web-based platform is all about letting students freely share their thoughts on various topics. USePinion uses smart hash maps to keep things organized and user-friendly. It&apos;s not just a place to express opinions; it&apos;s a vibrant community where every voice matters. Join us in making opinions heard and discussions lively. USePinion: Your space, your voice, your opinions!
                 </p>
+            </div>
+
+            <div>
+                <h4>Technologies used:</h4>
+
+
             </div>
         </main>
      );
