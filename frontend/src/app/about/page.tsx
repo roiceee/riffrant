de05ef3d7abe88aec5737f34860b15ea91a00baa
kimzii <kimzietorres@gmail.com>
2023-12-18 01:52:16 +0800

    centered images

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -10,7 +10,7 @@ function AboutPage() {
 
 
     return ( 
-        <main className="prose mx-auto p-4">
+        <main className="prose mx-auto p-4 border-2">
             <div>
                 <h2 className="font-bold text-center">Welcome to USePinion</h2>
                 <p className="text-justify m-4">
@@ -22,27 +22,27 @@ function AboutPage() {
                 <h4 className="p-4">Technologies used:</h4>
                 <div className="flex space-x-4  w-100" >
                     <div className="flex flex-row items-center ml-14 justify-between text-sm">
-                        <div className="m-4 flex flex-col items-center">
+                        <div className="m-5 flex flex-col items-center justify-center">
                             <Image className="m-0" src={nextjs} width={40} height={40} alt="no image" />
                             <p className="m-1">Next.Js</p>
                         </div>
                             
-                        <div className="m-4 flex flex-col items-center">
+                        <div className="m-5 flex flex-col items-center justify-center">
                             <Image className="m-0" src={typescript} width={40} height={40} alt="no image" />
                             <p className="m-1">TypeScript</p>
                         </div>
 
-                        <div className="m-4 flex flex-col items-center">
+                        <div className="m-5 flex flex-col items-center justify-center">
                             <Image className="m-0" src={spring} width={40} height={40} alt="no image" />
                             <p className="m-1">Spring</p>
                         </div>
 
-                        <div className="m-4 flex flex-col items-center">
+                        <div className="m-5 flex flex-col items-center justify-center">
                             <Image className="m-0" src={java} width={40} height={40} alt="no image" />
                             <p className="m-1">Java</p>
                         </div>
 
-                        <div className="m-4  flex flex-col items-center">
+                        <div className="m-5  flex flex-col items-center justify-center">
                             <Image className="m-0" src={postgresql} width={40} height={40} alt="no image" />
                             <p className="m-1">PostgreSQL</p>                          
                         </div>
