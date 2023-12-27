1f806044aace3c46630eb8515054956b7c74cdd0
jroiceee <jroicealdeza@gmail.com>
2023-12-27 12:58:09 +0800

    add catch clause

--- core/src/lib/server-actions.ts
+++ core/src/lib/server-actions.ts
@@ -2,9 +2,36 @@
 
 import LimiterStatus from "@/types/limiter-status";
 
-export async function checkPostRateLimit(key: string) : Promise<LimiterStatus | undefined> {
+export async function checkPostRateLimit(
+  key: string
+): Promise<LimiterStatus | undefined> {
+  try {
+    const res = await fetch(
+      `${process.env.RATE_LIMITER_URL}/post`,
+
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          creatorId: key,
+        }),
+      }
+    );
+
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    return undefined;
+  }
+}
+
+export async function checkVoteRateLimit(
+  key: string
+): Promise<LimiterStatus | undefined> {
   const res = await fetch(
-    `${process.env.RATE_LIMITER_URL}/post`,
+    `${process.env.RATE_LIMITER_URL}/vote`,
 
     {
       method: "POST",
@@ -20,22 +47,3 @@ export async function checkPostRateLimit(key: string) : Promise<LimiterStatus | 
   const data = await res.json();
   return data;
 }
-
-export async function checkVoteRateLimit(key: string) : Promise<LimiterStatus | undefined> {
-    const res = await fetch(
-      `${process.env.RATE_LIMITER_URL}/vote`,
-  
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          creatorId: key,
-        }),
-      }
-    );
-  
-    const data = await res.json();
-    return data;
-  }
